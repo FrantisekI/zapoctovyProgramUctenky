148e44d63b5553bb64408b8e3dcc8d4405d7d3de
FrantisekI <148909995+FrantisekI@users.noreply.github.com>
2024-12-02 21:44:58 +0100

    ubdated database to read foodnames

--- database.py
+++ database.py
@@ -60,3 +60,21 @@ finally:
     conn.commit()
     cursor.close()
     conn.close()
+
+def readFoodNames():
+    conn = mysql.connector.connect(
+        host=os.getenv('DB_HOST'),
+        user=os.getenv('DB_USER'),
+        password=os.getenv('DB_PASSWORD'),
+        database=os.getenv('DB_DATABASE')
+    )
+
+    cursor = conn.cursor()
+    cursor.execute("SELECT product_name FROM ReceiptItems")
+    foodNames = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return foodNames
+
+if __name__ == "__main__":
+    print(readFoodNames())
